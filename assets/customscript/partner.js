c51172b9c8d143c9afbe11d12c3a266ab58a23bf
sugavanesh <sugavaneshwaran.boopalan@i-exceed.com>
2020-11-18 14:57:27 +0530

    partner page

--- assets/customscript/partner.js
+++ assets/customscript/partner.js
@@ -50,3 +50,14 @@
 				}
 			}
 		});
+		
+				function sendmail() {
+			alert("Function is under development")
+		}
+$(document).ready(function() {
+    $('body').css("overflow-x", "hidden !important");
+  
+    $("#header").load("header.html");
+    $("#footer").load("footer.html");
+	//initiateSwiper();
+});
