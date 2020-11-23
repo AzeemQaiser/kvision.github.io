b9eb2bda35f23d3ae05e2969dd553244bdd39329
sugavanesh <sugavaneshwaran.boopalan@i-exceed.com>
2020-11-23 22:52:20 +0530

    testimonial slider fix

--- assets/customscript/partner.js
+++ assets/customscript/partner.js
@@ -1,28 +1,3 @@
- var swiper = new Swiper('.swiper1', {
-	   effect: 'flip',
-        navigation: {
-           nextEl: '.downarrow',
-           prevEl: '.uparrow',
-         },
-		  breakpoints: {
-         // when window width is >= 320px
-          640: {
-              slidesPerView: 1.1,
-             spaceBetween: 0,
-			 direction: 'horizontal'
-           },
-           768: {
-             slidesPerView: 1.1,
-             spaceBetween: 0,
-			 direction: 'horizontal'
-           },
-           992: {
-            slidesPerView: 1,
-            spaceBetween: 0,
-			direction: 'vertical'
-           }
-         }
-    });
   var swiper = new Swiper('.logo-carousal-container', {
 			slidesPerView: 1,
 			spaceBetween: 10,
